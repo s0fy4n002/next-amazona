90510e04cef7d8aed99b146381532f835aa94256
diana mardiana <dianadinn41@gmail.com>
2022-03-10 15:23:49 +0700

    cors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  api: {
+    responseLimit: false,
+    externalResolver: true,
+  },
+};
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,12 +1,22 @@
 import nc from 'next-connect';
 import db from '../../../utils/db';
 import Product from '../../../models/Product';
+import NextCors from 'nextjs-cors';
 
 const handler = nc();
+
 handler.get(async (req, res) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ['POST', 'DELETE'],
+    origin: 'http://127.0.0.1:3000',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   await db.connect();
   const products = await Product.find({});
   await db.disconnect();
+
   res.send(products);
 });
 
